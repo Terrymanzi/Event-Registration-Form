090045665ce4e1226c4941f2c0f4da7972d8cf28
Terry <156342254+Terrymanzi@users.noreply.github.com>
2024-06-19 16:20:06 +0200

    Fixed error with id name in index.html

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
+document.getElementById('registrationform').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the form from submitting
 
+    let isValid = true;
+
+    //name validation
+    const name = document.getElementById('name').value;
+    const nameRegrex = /^[A-Za-z\s]+$/;
+    if (!nameRegex.test(name)) {
+        alert('Name can only contain letters and spaces.');
+        isvalid = false;
+    }
+
+    //email validation
+    const email = document.getElementById('email') 
+}
